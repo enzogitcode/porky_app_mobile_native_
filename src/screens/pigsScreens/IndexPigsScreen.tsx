125e0f75c706f31101a165bf8775a297abfbcbe6
enzogit <enzo_91_10@users.noreply.github.com>
2026-01-25 18:48:06 -0300

    Pigs

--- src/screens/pigsScreens/IndexPigsScreen.tsx
+++ src/screens/pigsScreens/IndexPigsScreen.tsx
@@ -3,14 +3,19 @@ import React from "react";
 import Container from "../../ui/Container";
 import ButtonCustom from "../../ui/ButtonCustom";
 import { customStyles } from "../../styles/customStyles";
+import { useNavigation } from "@react-navigation/native";
+
+type IndexPigsScreenProps = ''
 
 const IndexPigsScreen = () => {
+  const navigation = useNavigation()
   return (
     <Container>
       <ButtonCustom
         btnStyle={[customStyles.menuButton, customStyles.btnStyle]}
         btnTitleStyle={customStyles.btnText}
         title="Ver Lista de cerdos"
+        onPress={() => navigation.navigate('PigsList')}
       />
       <ButtonCustom
         btnStyle={[customStyles.menuButton, customStyles.btnStyle]}
